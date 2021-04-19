fd872e3642d55660ffda4ca981bf6ff8f593334f
Naveen Kumar K <cbenaveen@gmail.com>
2021-04-19 06:46:05 +0530

    Converted API endpoint to POST from GET

--- src/main/java/com/naveen/microservice/wordwrap/controller/WordWrapController.java
+++ src/main/java/com/naveen/microservice/wordwrap/controller/WordWrapController.java
@@ -7,7 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,8 +23,8 @@ public class WordWrapController {
 
     private final WordWrapService wordWrapService;
 
-    @GetMapping(value = BASE_API_BATH, consumes = "application/json")
-    private ResponseEntity<WrappedResponse> test(@Valid @RequestBody final ContentRequest contentRequest) {
+    @PostMapping(value = BASE_API_BATH, consumes = "application/json")
+    private ResponseEntity<WrappedResponse> processContentWrap(@Valid @RequestBody final ContentRequest contentRequest) {
         log.info("Request received with Paragraph object {}", contentRequest);
 
         Collection<String> wrap = (Objects.isNull(contentRequest.getMaxLength()))
